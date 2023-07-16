7ebe0cd8361fc159d623c3212be1795fc6f0572d
afalkon <info.dryguy@gmail.com>
2023-07-16 22:52:57 +0200

    Clinician Calendar added

--- html/app/Http/Controllers/Company/Roles/Clinicians/CalendarController.php
+++ html/app/Http/Controllers/Company/Roles/Clinicians/CalendarController.php
@@ -9,8 +9,25 @@ use Illuminate\Http\Request;
 class CalendarController extends Controller
 {
     public function get_appointments() {
-        $user_id = auth()->user()->id;
-        $appointments = User::find($user_id)->clinician_appointments()->get();
+        $clinician_id = auth()->user()->id;
+        $clinician_instance = User::find($clinician_id);
+        $appointments = $clinician_instance->clinician_appointments()->
+                            with(['clinician' => function($q) {
+                                $q->select('id', 'name');
+                            }])->with(['patient' => function($q){
+                                $q->select('id', 'name', 'patient_id');
+                            }])->with(['clinic' => function($q){
+                                $q->select('id', 'city', 'address');
+                            }])->get();
+        // $appointments = $clinician_instance->with(['clinician_appointments' => function($q){
+        //     $q->with(['clinician' => function($q) {
+        //         $q->select('id', 'name');
+        //     }])->with(['patient' => function($q){
+        //         $q->select('id', 'name', 'patient_id');
+        //     }])->with(['clinic' => function($q){
+        //         $q->select('id', 'city', 'address');
+        //     }]);
+        // }])->get();
         return response()->json($appointments);
     }
 }
